9f1bd06538e696f362279637fca59bedce0202e8
Petronald Green <petronaldgreen@gmail.com>
2019-05-28 11:28:17 -0400

    allow blank UrlProfile values in admin
    + auto include migrations

--- shortener/models.py
+++ shortener/models.py
@@ -15,16 +15,16 @@ class UrlMap(models.Model):
     date_expired = models.DateTimeField()
 
     def __str__(self):
-        return '{}-{}'.format(self.user, self.full_url)
+        return '{} - {}'.format(self.user, self.full_url)
 
 
 class UrlProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     enabled = models.NullBooleanField(default=True, null=True)
-    max_urls = models.IntegerField(default=-1, null=True)
-    max_concurrent_urls = models.IntegerField(default=100, null=True)
-    default_lifespan = models.IntegerField(default=120, null=True)
-    default_max_uses = models.IntegerField(default=-1, null=True)
+    max_urls = models.IntegerField(default=-1, null=True, blank=True)
+    max_concurrent_urls = models.IntegerField(default=100, null=True, blank=True)
+    default_lifespan = models.IntegerField(default=120, null=True, blank=True)
+    default_max_uses = models.IntegerField(default=-1, null=True, blank=True)
 
     def __str__(self):
-        return self.user
+        return '{}'.format(self.user)
