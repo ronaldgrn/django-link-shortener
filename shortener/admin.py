0e7a824ad308c6fafeae20aad4743f405e1da76b
Denis Senchishen <dsenchishen@icloud.com>
2023-01-04 20:03:44 +0200

    Updated UrlMap admin page

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 from .models import UrlMap, UrlProfile
 
+
+class UrlMapAdmin(admin.ModelAdmin):
+    model = UrlMap
+    raw_id_fields = (
+        "user",
+    )
+    list_display = (
+        "short_url",
+        "user",
+        "usage_count",
+        "max_count",
+        "lifespan",
+        "date_created",
+        "date_expired",
+        "full_url",
+    )
+    search_fields = (
+        "short_url",
+        "full_url",
+        "user__username",
+    )
+
+
 # Register your models here.
-admin.site.register(UrlMap)
+admin.site.register(UrlMap, UrlMapAdmin)
 admin.site.register(UrlProfile)
