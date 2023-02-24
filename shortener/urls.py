f94a58ad71ca10988b66b1573fa89010f27e2408
Sebastian Muthwill <sebastian@sebserver.de>
2023-02-24 08:36:05 +0100

    adds app_name and patern names
    app name and pattern names are necessary to reverse urls in other modules.

--- shortener/urls.py
+++ shortener/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from shortener import views
 from django.conf import settings
 
+app_name = "shortener"
 urlpatterns = [
-    path('<link>/', views.expand),
+    path('<link>/', views.expand, name="expand"),
 ]
 
 if getattr(settings, 'SHORTENER_ENABLE_TEST_PATH', False):
-    urlpatterns.append(path('test/<path:link>', views.test))
+    urlpatterns.append(path('test/<path:link>', views.test, name="test"))
