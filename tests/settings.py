18c1ae80766c32f0f18993b29b42825041f540d0
Petronald Green <petronaldgreen@gmail.com>
2025-06-15 14:19:21 +0100

    set DEFAULT_AUTO_FIELD in tests/settings.py to silence warnings

--- tests/settings.py
+++ tests/settings.py
@@ -122,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
